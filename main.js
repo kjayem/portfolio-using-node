91ff5d08a58a7b3f685b13374534cc1607c9e0dc
kjayem <flwfeeld@gmail.com>
2021-08-20 17:13:51 +0900

    added home page router

--- main.js
+++ main.js
@@ -1,28 +1,16 @@
 const express = require('express');
 const app = express();
-const homeTemplate = require('./public/js/home-template.js');
-const contactTemplate = require('./public/js/contact-template');
+const index = require("./routes/index");
 const memories = require("./routes/memories");
-
-
 
 //public 안에있는 정적인 파일들을 사용하기 위함
 app.use(express.static('public'));
 
-//use the projects.js file to handle endpoints that start with /projects
-app.use("/memories", memories);
-
-
 //homepage
-app.get('/', (req, res) => {
-    var currentTitle = 'NAMEHERE';
-    // var about = aboutTemplate.aboutTemplate('Joonmin Kweon', 'Music Producer / Creative Artist');
-    // var projects = projectTemplate.projectTemplate();
-    // var memories = memoriesTemplate.memoriesTemplate();
-    var contact = contactTemplate.contactTemplate('flwfeeld@gmail.com', '+82 01029347310', '@j00my', 'Mullae-dong, Yeongdeungpo-gu, Seoul, S. Korea');
-    var html = homeTemplate.htmlHome(currentTitle, contact);
-    res.send(html);
-});
+app.use("/", index);
+
+//memories page
+app.use("/memories", memories);
 
 //error handlers
 app.use(function(req, res, next) {
