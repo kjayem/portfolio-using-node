ff4a2f25fa206c4b7adc6e2ace4a816e1ab94b0a
kjayem <flwfeeld@gmail.com>
2021-12-11 02:14:54 +0900

    stuff

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var indexRouter = require("./routes/index");
 var aboutRouter = require("./routes/about");
 var projectsRouter = require("./routes/projects");
 var scrapbookRouter = require("./routes/scrapbook");
-var noticeRouter = require("./routes/notice");
+// var noticeRouter = require("./routes/notice");
 
 var app = express();
 
@@ -27,7 +27,7 @@ app.use("/", indexRouter);
 app.use("/about", aboutRouter);
 app.use("/scrapbook", scrapbookRouter);
 app.use("/projects", projectsRouter);
-app.use("/notice", noticeRouter);
+// app.use("/notice", noticeRouter);
 
 //error handlers
 app.use(function(req, res, next) {
